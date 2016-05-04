7ffc5199f28ff1708f09bc65fa40e7ac62c3883a
Raul Stelescu <stelescuraul@gmail.com>
2016-05-04 16:40:49 +0200

    added reservation class

--- src/assignment4/Reservation.java
+++ src/assignment4/Reservation.java
@@ -5,14 +5,48 @@
  */
 package assignment4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  *
  * @author raul
  */
-public class Reservation implements ReservationInterface{
+public class Reservation implements ReservationInterface {
+
+    private static Reservation instance;
+    private static Connection conn = null;
+    private static PreparedStatement stmt;
+    private String URL = 'datdb.cphbusiness.dk';
+    private String user;
+    private String pw;
+
+    public Reservation(String user, String pw) throws SQLException {
+        this.user = user;
+        this.pw = pw;
+        getConnection();
+    }
+
+    private Connection getConnection() throws SQLException {
+        conn = DriverManager.getConnection(URL, user, pw);
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+    public static PreparedStatement prepare(String SQLString) {
+        try {
+            stmt = conn.prepareStatement(SQLString);
+        } catch (SQLException e) {
+            System.out.println("Error in DB.prepare()" + e);
+        }
+        return stmt;
+    }
     
-    public Reservation(){
-        
+    public void closeConnection() throws SQLException{
+        conn.close();
     }
 
     @Override
@@ -44,5 +78,5 @@ public class Reservation implements ReservationInterface{
     public boolean isAllReserved(String plane_no) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
